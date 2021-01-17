a09a4ce4d22789ae54f0d3f201bf6c0c7c22dd2a
Michel Emad Wakim <micho.courses@gmail.com>
2021-01-17 20:29:34 +0200

    creating 2 buttons one for the customer and other for the driver

--- app/src/main/java/android/example/uber_like/MainActivity.java
+++ app/src/main/java/android/example/uber_like/MainActivity.java
@@ -1,13 +1,30 @@
 package android.example.uber_like;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //creating the 2 buttons
+    private Button mDriver, mCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //relating the the private buttons to the GUI buttons
+        mDriver = (Button) findViewById(R.id.driver);
+        mCustomer = (Button) findViewById(R.id.customer);
+
+        //function to open a new view by clicking the button
+        mDriver.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+            startActivity(intent);
+            finsih();
+        });
     }
 }
